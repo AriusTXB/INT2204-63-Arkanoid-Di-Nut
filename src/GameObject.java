75534ad4670e9ef2faacce994d6eacf76ad70971
DungPhanHoangg05 <143725969+DungPhanHoangg05@users.noreply.github.com>
2025-09-29 21:51:32 +0700

    GameObject, MovableObject

---
+++ src/GameObject.java
@@ -0,0 +1,16 @@
+package Project;
+
+abstract class GameObject {
+    protected float x, y;
+    protected float width, height;
+
+    public GameObject(float x, float y, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void update();
+    public abstract void render();
+}
